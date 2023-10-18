d7be17eddc364f162308bca18390fcfd362a8f96
Amr Ragab <78859252+AmrRaggab@users.noreply.github.com>
2023-10-19 01:14:05 +0300

    Add files via upload

---
+++ ETL-Telecom-Project/sql_files/SQLQuery2.sql
@@ -0,0 +1,12 @@
+select * from [dbo].[fact_transaction]
+
+select * from [dbo].[err_destination_output]
+
+select * from [dbo].[err_source_output]
+
+select * from [dbo].[dim_audit]
+
+truncate table [dbo].[fact_transaction]
+truncate table [dbo].[err_destination_output]
+truncate table [dbo].[err_source_output]
+truncate table [dbo].[dim_audit]
